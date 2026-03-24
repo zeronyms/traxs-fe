ffafa856b2a4e3243236c1f6bf0743b49b1e7ab6
zeronyms <ksdarazi@gmail.com>
2026-03-24 21:04:58 +0700

    add: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-        Welcome to Trasxsss
+        Welcome to Traxs
       </h1>
     </div>
   );
